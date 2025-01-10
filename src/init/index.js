f3eb15e5892c8bcff4c171974c0d1bb4c483f169
조용필 <78397393+bbie-6772@users.noreply.github.com>
2025-01-10 15:24:06 +0900

    [add] 서버 시작 시 게임세션 1개 만들기 기능

--- src/init/index.js
+++ src/init/index.js
@@ -1,7 +1,9 @@
+import { createGame } from "../session/game.session.js";
 import { loadProtobufs } from "./loadProtobuf.js"
 
 export const initServer = async () => {
     try {
+        await createGame();
         await loadProtobufs();
     } catch (e) {
         console.error(e)
--- src/session/game.session.js
+++ src/session/game.session.js
@@ -2,9 +2,10 @@ import Game from "../classes/models/game.class.js";
 import { games } from "./session.js";
 
 
-export const createGame = () => {
+export const createGame = async () => {
     const game = new Game();
     games.set(game.id, game);
+    console.log("게임 인스턴스 생성 완료")
 
     return game
 }
